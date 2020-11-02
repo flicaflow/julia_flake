3d19fa0c83aef0eb7520e4991df39323e979e51b
Florian Schulze <florian@contextflow.com>
2020-11-02 13:56:30 +0100

    init, adds julia 1.5

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux =
+      with nixpkgs.legacyPackages.x86_64-linux;  {
+
+        julia_15 = stdenv.mkDerivation rec {
+          pname = "julia";
+          version = "1.5.2";
+
+          buildInputs = [ patchelf ];
+
+          src = fetchTarball {
+            url = "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-${version}-linux-x86_64.tar.gz";
+            sha256 = "0k3bpw8v07b70z50dyvjgz5wc58bnvw505lfcssczmhpfqd5zj49";
+          };
+
+          buildPhase = ''
+            ls
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -R . $out
+            chmod -R +x $out/bin
+            ls -l $out
+          '';
+
+          fixupPhase = ''
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/julia
+          '';
+        };
+
+        julia = julia_15;
+
+    };
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.julia;
+
+  };
+}
