b56b3d179cecc8f469fe0ce8d8f1000c08576d9b
Florian Schulze <florian@contextflow.com>
2020-11-03 13:47:23 +0100

    moved julia derivation to separate file

--- flake.nix
+++ flake.nix
@@ -1,37 +1,12 @@
 {
-  description = "A very basic flake";
+  description = "Flake packaging pre-build julia binaries";
 
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux =
       with nixpkgs.legacyPackages.x86_64-linux;  rec {
 
-        julia_15 = stdenv.mkDerivation rec {
-          pname = "julia";
-          version = "1.5.2";
-
-          buildInputs = [ patchelf ];
-
-          src = fetchTarball {
-            url = "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-${version}-linux-x86_64.tar.gz";
-            sha256 = "0k3bpw8v07b70z50dyvjgz5wc58bnvw505lfcssczmhpfqd5zj49";
-          };
-
-          buildPhase = ''
-            ls
-          '';
-
-          installPhase = ''
-            mkdir -p $out
-            cp -R . $out
-            chmod -R +x $out/bin
-            ls -l $out
-          '';
-
-          fixupPhase = ''
-            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/julia
-          '';
-        };
+        julia_15 = callPackage ./julia_15.nix {};
 
         julia = julia_15;
 
---
+++ julia_15.nix
@@ -0,0 +1,27 @@
+{stdenv, patchelf, ...}:
+stdenv.mkDerivation rec {
+  pname = "julia";
+  version = "1.5.2";
+
+  buildInputs = [ patchelf ];
+
+  src = fetchTarball {
+    url = "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-${version}-linux-x86_64.tar.gz";
+    sha256 = "0k3bpw8v07b70z50dyvjgz5wc58bnvw505lfcssczmhpfqd5zj49";
+  };
+
+  buildPhase = ''
+    ls
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -R . $out
+    chmod -R +x $out/bin
+    ls -l $out
+  '';
+
+  fixupPhase = ''
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/julia
+  '';
+}
