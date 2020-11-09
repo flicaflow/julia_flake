5750b647668bcca73eda543ea3c64f310fcbaf5a
Florian Schulze <florian@contextflow.com>
2020-11-09 13:33:50 +0100

    adds buildJulia15Package helper function

--- flake.nix
+++ flake.nix
@@ -3,14 +3,27 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux =
+    packages =
       with nixpkgs.legacyPackages.x86_64-linux;  rec {
 
-        julia_15 = callPackage ./julia_15.nix {};
+        x86_64-linux.julia_15 = callPackage ./julia_15.nix {};
 
-        julia = julia_15;
+        x86_64-linux.julia = self.packages.x86_64-linux.julia_15;
+
 
     };
+
+    lib.buildJulia15Package = 
+      with nixpkgs.legacyPackages.x86_64-linux; 
+      callPackage ./build-julis-15-package.nix { inherit (self.packages.x86_64-linux) julia_15; };
+
+
+    apps.x86_64-linux.julia = {
+      type = "app";
+      program = "${self.defaultPackage.x86_64-linux}/bin/julia";
+    };
+
+    defaultApp.x86_64-linux = self.apps.x86_64-linux.julia;
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.julia;
 
